36a9ff1e10ed2bfe7e24028000cc269ffc4443be
Phillip <p_dijo@hotmail.com>
2016-01-04 23:31:34 -0800

    Starting over on TicTacToe script

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,53 +2,35 @@ var msg = function(text) {
   console.log(text);
 };
 
+//////////////////////
+// Global Variables //
+//////////////////////
+
 var alertBox = $(".alert"); // Quick alert section grab
 
-// Starts the object 
+////////////////
+// The Object //
+////////////////
+
+/*
+Starts the TicTacToe object.
+"player" sets the player value to 1 so 
+that we can differentiate it from the computer's value.
+"board" sets up an empty board.
+"wins" shows the possible wins using the array index, 
+starting with rows, then columns, then diagonals.
+"playerIs" and "computerIs" will hold the text for who 
+is O and who is X while using the FontAwesome Icons.
+*/
 
 var TicTacToe = {
   player: 1,
   computer: 2,
-  board: [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ],
+  board: [0,0,0,0,0,0,0,0,0,],
+  wins: [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[1,4,8],[2,4,6]],
+  idNames: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"],
   playerIs: "",
-  computerIs: ""
-};
-
-// Checks for a win
-
-TicTacToe.checkForWin = function(playerNum, game) {
-
-  if ((game[0][0] === playerNum && game[0][1] === playerNum && game[0][2] === playerNum) ||
-    (game[1][0] === playerNum && game[1][1] === playerNum && game[1][2] === playerNum) ||
-    (game[2][0] === playerNum && game[2][1] === playerNum && game[2][2] === playerNum) ||
-    (game[0][0] === playerNum && game[1][0] === playerNum && game[2][0] === playerNum) ||
-    (game[0][1] === playerNum && game[1][1] === playerNum && game[2][1] === playerNum) ||
-    (game[0][2] === playerNum && game[1][2] === playerNum && game[2][2] === playerNum) ||
-    (game[0][0] === playerNum && game[1][1] === playerNum && game[2][2] === playerNum) ||
-    (game[0][2] === playerNum && game[1][1] === playerNum && game[2][0] === playerNum)) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-TicTacToe.checkForEnd = function() {
-
-  if (TicTacToe.board[0].indexOf(0) === -1 &&
-    TicTacToe.board[1].indexOf(0) === -1 &&
-    TicTacToe.board[2].indexOf(0) === -1) {
-
-    return true;
-
-  } else {
-
-    return false;
-
-  }
+  computerIs: "",
 };
 
 // Sets who is X and who is O
@@ -58,107 +40,71 @@ TicTacToe.setPlayer = function(player, computer) {
   TicTacToe.computerIs = computer;
 };
 
+// Checks for a win
+
+TicTacToe.checkForWin = function(game, player) {
+  //for loop to go through the different win conditions
+  // var match = false
+  // for loop to go through the different cells to check
+
+};
+
+// Checks for a draw
+
+TicTacToe.checkForDraw = function(game) {
+  return TicTacToe.board.indexOf(0) === -1 ? true : false ;
+};
+
 // Changes the array for the board based off what cell the player clicks
 
 TicTacToe.changeBoard = function(name, player) {
   switch (name) {
     case "one":
-      TicTacToe.board[0][0] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[0] = player;
       break;
     case "two":
-      TicTacToe.board[0][1] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[1] = player;
       break;
     case "three":
-      TicTacToe.board[0][2] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[2] = player;
       break;
     case "four":
-      TicTacToe.board[1][0] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[3] = player;
       break;
     case "five":
-      TicTacToe.board[1][1] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[4] = player;
       break;
     case "six":
-      TicTacToe.board[1][2] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[5] = player;
       break;
     case "seven":
-      TicTacToe.board[2][0] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[6] = player;
       break;
     case "eight":
-      TicTacToe.board[2][1] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[7] = player;
       break;
     case "nine":
-      TicTacToe.board[2][2] = player;
-      TicTacToe.checkForWin(player, TicTacToe.board);
-      TicTacToe.checkForEnd();
+      TicTacToe.board[8] = player;
       break;
     default:
       break;
   }
 };
 
-// The computer turn
-// TODO:
-//     1. Have computer calculate best path towards its victory using minimax (!!!Last)
-//     3. Have "O" always start no matter who is playing.
+// Converts the where the computer wants to go to an ID name for the table
 
-TicTacToe.convertToID = function(row, col) {
-  var idNames = [
-    ["one", "two", "three"],
-    ["four", "five", "six"],
-    ["seven", "eight", "nine"]
-  ];
-
-  return idNames[row][col];
+TicTacToe.convertToID = function(num) {
+  
+  return TicTacToe.idNames[num];
 };
 
-TicTacToe.computerTurn = function() {
-  var row = Math.floor(Math.random() * 3);
-  var col = Math.floor(Math.random() * 3);
-  var counter = 0;
-
-  test = TicTacToe.minMax(TicTacToe.board, TicTacToe.computer, 1);
-
-  // msg(test);
-
-  if (TicTacToe.board[row][col] !== 0) {
-    counter++;
-
-    if (counter < 100) {
-      TicTacToe.computerTurn();
-    } else {
-      //msg("Computer can't go anymore"); // to stop recursion during testing
-    }
-  } else {
-    var placement = TicTacToe.convertToID(row, col);
-    $("#" + placement).html("<i class='" + TicTacToe.computerIs + "'></i>");
-    TicTacToe.changeBoard(placement, TicTacToe.computer);
-
-  }
-};
-
-// Returns max value
+// Returns max value from array
 
 TicTacToe.max = function(arr) {
-  var max = [];
+  var max = -99999;
 
   arr.forEach(function(num) {
-    if (num[0] > max) {
+    if (num > max) {
       max = num;
     } else {
       max = max;
@@ -168,13 +114,13 @@ TicTacToe.max = function(arr) {
   return max;
 };
 
-// Returns min value
+// Returns min value from array
 
 TicTacToe.min = function(arr) {
-  var min = [];
+  var min = 99999;
 
   arr.forEach(function(num) {
-    if (num[0] < min) {
+    if (num < min) {
       min = num;
     } else {
       min = min;
@@ -184,88 +130,9 @@ TicTacToe.min = function(arr) {
   return min;
 };
 
-// Score Test for console use
-
-TicTacToe.scoreTest = function(board) {
-  if (TicTacToe.checkForWin(2, board) || (board[0].indexOf(0) === -1 && board[1].indexOf(0) === -1 && board[2].indexOf(0) === -1)) { // maximize for computer win
-    return 10;
-  }
-  if (TicTacToe.checkForWin(1, board) || (board[0].indexOf(0) === -1 && board[1].indexOf(0) === -1 && board[2].indexOf(0) === -1)) { // minimize for player win
-    return -10;
-  } else { // For draw
-    return 0;
-  }
-};
-
-// Checks to see if there are no more spaces
-
-TicTacToe.spacesLeft = function(board) {
-    return (board[0].indexOf(0) === -1 && board[1].indexOf(0) === -1 && board[2].indexOf(0) === -1) ? true : false;
-};
-
-// Minimax Function
-
-TicTacToe.minMax = function(board, player, depth) {
-
-  ////////////////////////////
-  // MINIMAX IMPLEMENTATION //
-  ////////////////////////////
-
-  if (TicTacToe.checkForWin(2, board) || TicTacToe.spacesLeft(board) || depth === 6) { // maximize for computer win
-    return 10 - depth;
-  } else if (TicTacToe.checkForWin(1, board) || TicTacToe.spacesLeft(board) || depth === 6) { // minimize for player win
-    return depth - (-10);
-  } else if (TicTacToe.spacesLeft(board) || depth === 6) { // For draw if ((board[0].indexOf(0) === -1 && board[1].indexOf(0) === -1 && board[2].indexOf(0) === -1) && depth <= 6)
-    return 0;
-  }
-
-  var tempBoard = board;
-
-  var scores = [];
-
-  for (var i = 0; i < tempBoard.length; i++) {
-
-    for (var j = 0; j < tempBoard[i].length; j++) {
-
-      if (tempBoard[i][j] === 0) {
-
-        tempBoard[i][j] = player;
-
-        if (player === 2) {
-          scores.push([TicTacToe.minMax(tempBoard, TicTacToe.player, depth + 1), i, j]);
-
-        } else {
-          scores.push([TicTacToe.minMax(tempBoard, TicTacToe.computer, depth +1), i, j]);
-
-        }
-
-      }
-
-    }
-
-  }
-  var bestScore;
-
-  console.log(scores, "after bestScore var");
-
-  if (player === 1) {
-    bestScore = TicTacToe.min(scores);
-  } else {
-    bestScore = TicTacToe.max(scores);
-  }
-
-  var x = bestScore[1]; // grabs X position from best score array
-  var y = bestScore[2]; // grabs Y position from best score array
-
-  return [x, y];
-
-};
-
-///////////////////////////////////
-// END OF MINIMAX IMPLEMENTATION //
-///////////////////////////////////
-
-// Click event to play the game
+//////////////////////
+// The click events //
+//////////////////////
 
 $("td").click(function(event) {
   var tdName = event.currentTarget.id;
@@ -323,3 +190,12 @@ $("#x").click(function() {
   }
 
 });
+
+/*
+
+TicTacToe.computerTurn
+TicTacToe.minMax
+TicTacToe.checkForWin
+TicTacToe.reset
+
+*/
